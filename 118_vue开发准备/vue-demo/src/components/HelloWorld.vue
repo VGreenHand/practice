78e9856bebdb7ba941cb6a23370dacf75ff1ee33
VGreenHand <914232759@qq.com>
2025-03-18 17:33:00 +0800

    更新 HelloWorld 组件，添加按钮、条件渲染和列表展示功能

--- 118_vue开发准备/vue-demo/src/components/HelloWorld.vue
+++ 118_vue开发准备/vue-demo/src/components/HelloWorld.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="hello">
-    <h1>学习vue：模板语法</h1>
+    <h1>学习vue</h1>
     <p>{{ message }}</p>
-    <div>{{ rawHtml }}</div>
     <div v-html="rawHtml"></div>
     <div :id="ID"></div>
+    <button @click="increment" @contextmenu.prevent="clearCount">点击:{{ count }}</button>
+    <!-- v-if -->
+    <p v-if="flag">Im Batman</p>
+    <p v-else>Im not Batman</p>
+    <!-- v-show -->
+    <p v-show="flag">Im Batman</p>
+    <p v-show="!flag">Im not Batman</p>
+    <ul>
+      <li v-for="item in newList" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,10 +26,27 @@ export default {
     return {
       message: 'Hello Vue!',
       rawHtml: "<a href = 'http://www.baidu.com'>这是一个链接</a>",
-      id: 'test'
+      id: 'test',
+      flag: false,
+      newList:[
+        {id:1,name:'张三'},
+        {id:2,name:'李四'},
+        {id:3,name:'王五'},
+      ],
+      count: 0
+
     }
+  },
+  methods: {
+          increment() {
+            this.count++
+          },
+          clearCount() {
+            this.count = 0
+          }
   }
 }
+
 </script>
 
 
