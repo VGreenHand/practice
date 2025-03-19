758c152c883b56d8324d068e9fb1ec0c44721588
VGreenHand <914232759@qq.com>
2025-03-19 17:03:09 +0800

    初始化 Vue 2 项目，添加基本配置文件、组件和服务工作者支持

---
+++ 118_vue开发准备/vue-2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <MyComponent :title="title" :age="age" @send-click="getDataHandle"/>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      title: 'This is a title',
+      age: 20,
+      msg:''
+    }
+  },
+  components: {
+    MyComponent
+  },
+  methods: {
+    getDataHandle(data) {
+      this.msg = data
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 118_vue开发准备/vue-2/src/components/MyComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <h3>这是一个组件示范</h3>
+    <p>{{ title }}</p>
+    <p>age = {{ age }}</p>
+    <button @click="sendClickHandel">click</button>
+</template>
+
+<script>
+    export default {
+        name: 'MyComponent',
+        props: {
+            title: {
+                type: String,
+                default: '默认标题'
+            },
+            age: {
+                type: Number,
+                default: 18
+            }
+        },
+        data() {
+            return {
+                message: 'This is MyComponent data'
+            }
+        },
+        methods: {
+            sendClickHandel(data) {
+                this.$emit('send-click', this.message)
+            }
+        }
+    }
+</script>
+
+<style>
+    h3 {
+        margin: 40px 0 0;
+        color: red;
+    }
+</style>
