301b1160856dd931745a8b157919e58580a17691
VGreenHand <914232759@qq.com>
2025-03-20 11:47:49 +0800

    初始化 swiper 插件，添加基本配置文件、组件和服务工作者支持

---
+++ 128_轮播第三方插件/swiper/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+<Swiper
+    :slides-per-view="1"
+    :space-between="50"
+    :modules="modules"
+    :pagination="{ clickable: true }"
+    :autoplay="{ delay: 1000, disableOnInteraction: false, pauseOnMouseEnter:true }"
+  >
+    <SwiperSlide><img src="../assets/logo.png" alt=""></SwiperSlide>
+    <SwiperSlide><img src="../assets/logo.png" alt=""></SwiperSlide>
+    <SwiperSlide><img src="../assets/logo.png" alt=""></SwiperSlide>
+    <SwiperSlide><img src="../assets/logo.png" alt=""></SwiperSlide>
+  </Swiper>
+</template>
+
+<script>
+import { Pagination,Autoplay } from 'swiper';
+import {Swiper,SwiperSlide} from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+        modules: [Pagination,Autoplay]
+    }
+  },
+  components: {
+    Swiper,
+    SwiperSlide
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
